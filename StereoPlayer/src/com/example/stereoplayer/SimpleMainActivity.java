d5f26da6f424e917fc5fef3dcc2a480b38614f1a
Weing Kai Wee <wngkai.91@gmail.com>
2013-04-02 10:43:16 -0700

    integrated stereo with play pause

--- StereoPlayer/src/com/example/stereoplayer/SimpleMainActivity.java
+++ StereoPlayer/src/com/example/stereoplayer/SimpleMainActivity.java
@@ -1,10 +1,15 @@
 package com.example.stereoplayer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.example.stereoplayer.MyApplication.SocketSend;
 
@@ -15,6 +20,7 @@ import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Point;
@@ -38,9 +44,247 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class SimpleMainActivity extends Activity implements OnGestureListener {	
+	
+	public class TCPReadTimerTask extends TimerTask 
+	{		
+		public void run() 
+		{
+			Log.i("Prog", "Read Timer Task started");
+
+			MyApplication app = (MyApplication) getApplication();
+			if (app.sock != null && app.sock.isConnected() && !app.sock.isClosed()) 
+			{
+				try 
+				{
+					InputStream in = app.sock.getInputStream();
+					int bytes_avail = in.available();
+					
+					if (bytes_avail > 0) 
+					{
+						Log.i( "Prog", bytes_avail + "bytes are available to be read" );
+						
+						byte buf[] = new byte[ONE_BYTE];
+						in.read(buf);
+						String msg = new String(buf, 0, ONE_BYTE, "US-ASCII");
+						String data = new String();
+
+						
+							if (msg.compareTo("M") == 0)
+							{
+								while ( in.available() == 0 );
+								bytes_avail = in.available();
+								
+								byte buffer[] = new byte[ONE_BYTE];
+								in.read(buffer);
+								Log.i("HandShake", "buffer is: " + buffer[0]);
+								int numBytesOfNumber = buffer[0];
+								
+								buffer = new byte[numBytesOfNumber];
+								
+								while( in.available() < numBytesOfNumber );
+								in.read(buffer);
+								Log.i("HandShake", "buffer[0] is: " + buffer[0]);
+								String temp = new String( buffer, 0, numBytesOfNumber, "US-ASCII" );
+								Log.i("HandShake", "temp is: " + temp );
+								
+								int numBytesOfData = Integer.parseInt( temp );
+								Log.i("HandShake", "numBytesOfData is: " + numBytesOfData );
+								
+								buffer = new byte[ONE_BYTE];
+								for ( int i = 0; i < numBytesOfData; )
+								{
+									if ( in.available() > 0 )
+									{
+										in.read(buffer);
+										Log.i("HandShake", "buffer[0] is: " + buffer[0]);
+										data = data.concat( new String(buffer, 0, ONE_BYTE, "US-ASCII") );
+										i++;
+									}
+								}
+								
+								Log.i("HandShake", "Data is: " + data);
+							
+						}
+						
+						final String command = new String(msg);
+						final String message = new String( data );
+
+						Log.i("HandShake", "String s is: " + command);
+						if (msg.compareTo("M") == 0)
+						{
+							Log.i("Shuffle", "String data is: " + data);
+							Log.i("Shuffle", "String message is: " + message);
+						}
+						
+						
+						runOnUiThread(new Runnable() 
+						{
+							public void run() 
+							{								
+								Log.i("Prog", "Started Run On UI Thread");
+
+								//TextView volumeT = (TextView) findViewById(R.id.viewText1);
+								//TextView text = (TextView) findViewById(R.id.viewText2);
+
+								if ( command.compareTo( "P" ) == 0 )
+								{
+									ImageView image = (ImageView) findViewById(R.id.mainButton);
+
+
+									image.setImageResource(R.drawable.pause_button);
+									showStatus.setText("Playing");
+									showStatus.show();
+									playing = true;
+								}
+								else if (command.compareTo("p") == 0) 
+								{
+									ImageView image = (ImageView) findViewById(R.id.mainButton);
+
+
+									image.setImageResource(R.drawable.play_button);
+									showStatus.setText("Paused");
+									showStatus.show();
+									playing = false;
+									
+								} 
+								else if (command.compareTo("S") == 0) 
+								{
+									//text.setText("Stoppped");
+								} 
+								else if (command.compareTo("U") == 0) 
+								{
+									//volumeT.setText("Volume = " + Integer.toString(volume));
+								} 
+								else if (command.compareTo("D") == 0) 
+								{
+									//volumeT.setText("Volume = " + Integer.toString(volume));
+								} 
+								/* Deprecated */
+								else if (command.compareTo("N") == 0) 
+								{
+									ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar1);
+									pb.setProgress( 0 );
+								}
+								/* Deprecated */
+								else if (command.compareTo("L") == 0) 
+								{
+									ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar1);
+									pb.setProgress( 0 );
+								} 
+								else if (command.compareTo("O") == 0) 
+								{
+									ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar2);
+									int songLength = Integer.parseInt(mainPlaylist.get(songIndex)[4]);
+									double progressInterval = 100.0 / songLength;
+									
+									currentSongPositionInTime += progressInterval;
+									bar.setProgress( (int) currentSongPositionInTime );
+									
+									//updateTime( currentSongPositionInTime, songLength );
+									
+									Log.i("Prog", "Progress increased by " + progressInterval );
+									Log.i("Prog", "currentSongPosition is: " + currentSongPositionInTime );
+									
+								}
+								else if (command.compareTo("M") == 0) 
+								{
+									
+									ImageView image = (ImageView) findViewById(R.id.mainButton);
+									TextView songName = (TextView) findViewById(R.id.songName);
+									TextView artistName = (TextView) findViewById(R.id.artistName);
+									ProgressBar bar = (ProgressBar) findViewById(R.id.progressBar2);
+									bar.setProgress( 0 );
+									songIndex = Integer.parseInt( message );
+									songName.setText(mainPlaylist.get(songIndex)[1]);
+									artistName.setText(mainPlaylist.get(songIndex)[2]);
+
+									image.setImageResource(R.drawable.pause_button);
+									showStatus.setText("Playing");
+									showStatus.show();
+									playing = true;
+									currentSongPositionInTime = 0;
+
+									//}
+									//else {
+									//	image.setImageResource(R.drawable.play_button);
+									//	showStatus.setText("Paused");
+									//	showStatus.show();
+									//	playing = false;
+									//}
+									/*
+									Log.i( "HandShake", "Successfully reached here" );
+									
+									ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar1);
+									pb.setProgress( 0 );
+									
+									songIndex = Integer.parseInt( message );
+									Log.i("indexNumber", Integer.toString(songIndex));
+									text.setText("Playing: " + mSongs[songIndex]);
+									
+									setupTime( Integer.parseInt( mLengths[songIndex] ) );
+									currentSongPositionInTime = 0;
+									*/
+								} 
+								else
+								{
+									
+								}
+							}
+						});
+					}
+					
+					/* Debugging purpose */
+//					if ( Debug )
+//					{
+//						String[] list = new String[100];
+//						for ( int i = 0; i < 100; i++ )
+//						{
+//							list[i] = Integer.toString( i );
+//						}
+//						
+//						/* playlist */
+//						Log.i( "list", "Before save playList" );
+//						savePlayList( "playList.txt", list );
+//						Log.i( "list", "After save playList and Before load PlayList" );
+//						
+//						list = loadPlayList( "playList.txt" );
+//						Log.i( "list", "After Load playList" );
+//						
+//						for ( int i = 0; i < list.length; i++ )
+//							Log.i( "list", list[i] );
+//						
+//						sendCurrentPlayListToDE2( list );
+//						
+//						/* rating */
+//						for ( int i = 0; i < 100; i++ )
+//						{
+//							list[i] = Integer.toString( 99-i );
+//						}
+//						
+//						Log.i( "list", "Before save saveRating" );
+//						saveRating( list );
+//						Log.i( "list", "After save saveRating and Before load saveRating" );
+//						
+//						list = loadRating( );
+//						Log.i( "list", "After Load saveRating" );
+//						
+//						for ( int i = 0; i < list.length; i++ )
+//							Log.i( "list", list[i] );
+//						
+//						Debug = false;
+//					}
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	
 	/* Set this to true if debugging saving and loading play list and/or rating list*/
 	boolean Debug = true;
@@ -58,10 +302,13 @@ public class SimpleMainActivity extends Activity implements OnGestureListener {
 	private GestureDetector gestureDetector;
 	private ArrayList<String[]> mainPlaylist;
 	private int mode;
+	private int songIndex;
+	private MyApplication app;
+	private int currentSongPositionInTime;
 
 	@SuppressLint("ShowToast")
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -76,7 +323,8 @@ public class SimpleMainActivity extends Activity implements OnGestureListener {
 		PositioningThread scale = new PositioningThread();
 		scale.run();
 		
-		MyApplication app = (MyApplication)SimpleMainActivity.this.getApplication();
+		MyApplication myApp = (MyApplication)SimpleMainActivity.this.getApplication();
+		app = myApp;
 		//app.ipStr = ipStr;
 		//app.portNumber = portNumber;
 		
@@ -253,7 +501,6 @@ public class SimpleMainActivity extends Activity implements OnGestureListener {
 
 	@Override
 	public boolean onDown(MotionEvent arg0) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
@@ -277,25 +524,21 @@ public class SimpleMainActivity extends Activity implements OnGestureListener {
 
 	@Override
 	public void onLongPress(MotionEvent arg0) {
-		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2,
 			float arg3) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void onShowPress(MotionEvent arg0) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public boolean onSingleTapUp(MotionEvent arg0) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 	
@@ -312,11 +555,16 @@ public class SimpleMainActivity extends Activity implements OnGestureListener {
 			if (resultCode == Activity.RESULT_OK) { 
 				
 				String[] playlist = data.getStringArrayExtra("FromLoading");
+				initializeList(playlist);
 				
-				for (int i= 0; i < playlist.length; i++)
+				TCPReadTimerTask tcp_task = new TCPReadTimerTask();
+				Timer tcp_timer = new Timer();
+				tcp_timer.schedule(tcp_task, 3000, 200);
+				
+				/*for (int i= 0; i < playlist.length; i++)
 				{
 					Log.i("inside", playlist[i]);
-				}
+				}*/
 				
 				//Toast.makeText(this, newText, Toast.LENGTH_SHORT).show();
 			} 
@@ -325,55 +573,267 @@ public class SimpleMainActivity extends Activity implements OnGestureListener {
 		} 
 	}
 	
-	// This is the Socket Connect asynchronous thread. Opening a socket
-	// has to be done in an Asynchronous thread in Android. Be sure you
-	// have done the Asynchronous Tread tutorial before trying to understand
-	// this code.
-	/*public class SocketConnect extends AsyncTask<Void, Void, Socket> {
-		// The main parcel of work for this thread. Opens a socket
-		// to connect to the specified IP.
-		protected Socket doInBackground(Void... voids) {
-			Socket s = null;
-			String ip = ipStr;
-			Integer port = portStr;
-			try {
-				s = new Socket(ip, port);
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return s;
+	public void initializeList(String[] playlist) {
+		
+		for (int i = 0; i + 5 <= playlist.length; i += 5) 
+		{
+			String[] newSong = new String[5];
+			newSong[0] = playlist[i];
+			newSong[1] = playlist[i + 1];
+			newSong[2] = playlist[i + 2];
+			newSong[3] = playlist[i + 3];
+			newSong[4]	= playlist[i + 4];
+			mainPlaylist.add(newSong);
 		}
+	}
+	
+	public void playPauseSong(View view) {
+		app.new SocketSend().execute("P");
+	}
 
-		// After executing the doInBackground method, this is
-		// automatically called, in the UI (main) thread to store
-		// the socket in this app's persistent storage
-		protected void onPostExecute(Socket s) {
-			MyApplication myApp = (MyApplication) SimpleMainActivity.this.getApplication();
-			myApp.sock = s;
+	public void stopSong(View view) {
+		app.new SocketSend().execute("S");
+	}
 
-			try {
-				Log.i( "Prog", "Clearing UART" );
+	public void nextSong(View view) {
+		app.new SocketSend().execute("N");
+	}
 
-				InputStream in = s.getInputStream();
-				int bytes_avail = in.available();
-				if (bytes_avail > 0) {
-					// If so, read them in and create a string
-					byte buf[] = new byte[bytes_avail];
-					in.read(buf);
+	public void prevSong(View view) {
+		app.new SocketSend().execute("L");
+	}
 
-				}
-			} catch (IOException e) {
-				Log.i( "Prog", "Clearing UART Failed" );
-				e.printStackTrace();
-			}
+	public void upVolume(View view) {
+		songVolume++;
+		app.new SocketSend().execute("U");
+	}
 
-			//if(mode == DE2)
-			//	new SocketSend().execute("playlist");
-			//else
-			//	new SocketSend().execute("02.01.Super MArio.kai.5.64.02.LOL.new artist.4.22.");
+	public void downVolume(View view) {
+		songVolume--;
+		app.new SocketSend().execute("D");
+	}
+	
+	public void onClickPlayOrderMode( View view )
+	{
+		boolean isShuffle = ((ToggleButton) view).isChecked();
+		
+		if ( isShuffle )
+			app.new SocketSend().execute("H");
+		else
+			app.new SocketSend().execute("h");
+	}
+	
+	public void onClickPlayRepeatMode( View view )
+	{
+		boolean isRepeatOneSong = ((ToggleButton) view).isChecked();
+		
+		if ( isRepeatOneSong )
+			app.new SocketSend().execute("R");
+		else
+			app.new SocketSend().execute("r");
+	}
+	
+	/* Sends a playList to DE2
+	 * returns 0 if successful, otherwise -1
+	 * Pre: playList != null
+	 */
+	public int sendCurrentPlayListToDE2( String[] playList )
+	{
+		MyApplication app = (MyApplication) getApplication();
+		InputStream in = null;
+		try 
+		{
+			in = app.sock.getInputStream();
+		} 
+		catch (IOException e1) 
+		{
+			Log.i( "Exception", "app.sock.getInputStream() failed in sendCurrentPlayListToDE2" );
+			return -1;
 		}
-	}*/
+		
+		int listLength = playList.length;
+		Log.i( "list", "Start sending playList" );
+		//new SocketSend().execute( Integer.toString( ( Integer.toString( listLength ) ).length() ) );
+		//Log.i( "list", "Sending: " +  Integer.toString( ( Integer.toString( listLength ) ).length() ) );
+		app.new SocketSend().execute( Integer.toString( listLength ) );
+		Log.i( "list", "Sending: " +  Integer.toString( listLength ) );
+		
+		for ( int i = 0; i < listLength; i++ )
+		{
+			if ( i != 0 && i % 30 == 0 )	// for HandShake
+			{
+				try 
+				{
+					byte buf[] = new byte[ONE_BYTE];
+										
+					app.new SocketSend().execute( "H" );
+					Log.i( "list", "Sending H" );
+					
+					
+					/* Android loopback mode purpose */
+					/*
+					String msg = new String();
+					while ( msg.compareTo( "H" ) != 0 )
+					{
+						if ( in.available() > 0 )
+						{
+							in.read( buf );
+							msg = new String(buf, 0, ONE_BYTE, "US-ASCII");
+						}
+					}
+					*/
+					
+					/* Real Purpose */
+					while ( in.available() == 0 );
+					Log.i( "list", "Got message from DE2" );
+					
+					in.read( buf );
+					String msg = new String(buf, 0, ONE_BYTE, "US-ASCII");
+					
+					Log.i( "list", "Message got is: " + msg );
+					
+					if ( msg.compareTo( "H" ) != 0 )
+					{
+						Log.i( "list", "Invalid message came from DE2 in sendCurrentPlayListToDE2" );
+						return -1;
+					}
+					Log.i( "list", "valid message came from DE2 in sendCurrentPlayListToDE2" );
+				} 
+				catch (IOException e) 
+				{
+					Log.i( "Exception", "IOException failed in sendCurrentPlayListToDE2" );
+					return -1;
+				}		
+			}
+				
+			//new SocketSend().execute( Integer.toString( playList[i].length() ) );
+			//Log.i( "list", "Sending: " +  Integer.toString( playList[i].length() ) );
+			app.new SocketSend().execute( playList[i] );
+			Log.i( "list", "Sending: " +  playList[i] );
+		}
+		Log.i( "list", "Done sending playList" );
+		return 0;
+	}
+	
+	/* Wrapper function for storing rating list */
+	public void saveRating( String[] rating )
+	{
+		saveList( "Rating.txt", rating );
+	}
+	
+	/* Wrapper function for loading rating list */
+	public String[] loadRating()
+	{
+		return loadList( "Rating.txt" );
+	}
+	
+	/* Wrapper function for saving play list */
+	public void savePlayList( String name, String[] str )
+	{
+		saveList( name, str );
+	}
+	
+	/* Wrapper function for loading play list */
+	public String[] loadPlayList( String name )
+	{
+		return loadList( name );
+	}
+	
+	/* Stores a list of string into internal storage as a file
+	 * @name is the name of the file
+	 * @str is the list of strings to store
+	 * Pre: Str != null
+	 */
+	public void saveList( String name, String[] str )
+	{
+		FileOutputStream fos = null;
+		
+		try 
+		{
+			fos = openFileOutput( name, Context.MODE_PRIVATE );
+			
+			for ( int i = 0; i < str.length; i++ )
+			{
+				fos.write( str[i].getBytes() );
+				fos.write( ".".getBytes() );
+			}
+		} 
+		catch (FileNotFoundException e) 
+		{
+			Log.i( "Exception", "File: " + name + " is not found." );
+		} 
+		catch (IOException e) 
+		{
+			Log.i( "Exception", "str.getBytes() threw an IOException for file: " + name + "." );
+		}
+		finally
+		{
+			try 
+			{
+				fos.close();
+			} 
+			catch (IOException e) 
+			{
+				Log.i( "Exception", "Failed close file: " + name + "." );
+			}
+		}
+	}
+	
+	/* Loads a list of string from the internal storage
+	 * @name is the name of the file
+	 */
+	public String[] loadList( String name )
+	{
+		FileInputStream fis = null;
+		try 
+		{
+			fis = openFileInput( name );
+			
+			byte[] buf = new byte[MAX_BYTES];
+			String temp = new String();
+			int i;
+			
+			int bytesRead;
+			while ( (bytesRead = fis.read( buf )) != -1 )
+			{
+				Log.i( "playList", "bytesRead is: " + bytesRead );
+				temp = temp.concat( new String( buf, 0, bytesRead, "US-ASCII" ));			
+			}
+			Log.i( "playList", "bytesRead is: " + bytesRead );
+			
+			String[] str = temp.split("\\.");
+			
+			for ( i = 0; i < str.length; i++)
+				Log.i( "ss", "str[" + i + " ]: " + str[i] );
+			
+			return str;
+		} 
+		catch (FileNotFoundException e) 
+		{
+			Log.i( "Exception", "File: " + name + " is not found." );
+		} 
+		catch (IOException e) 
+		{
+			Log.i( "Exception", "fis.read() threw an IOException for file: " + name + "." );
+		}
+		finally
+		{
+			try 
+			{
+				fis.close();
+			} 
+			catch (IOException e) 
+			{
+				Log.i( "Exception", "Failed close file: " + name + "." );
+			}
+		}
+		
+		return null;
+	}
+	
+	/* Debugging purpose */
+	public void debugHandShakedLongMessage(View view) {
+		app.new SocketSend().execute("M");
+	}
 
 }
